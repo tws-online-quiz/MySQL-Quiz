144ff4cb7343a26a0059dadde3fc490e56d19d83
Pei Zhang <pezhang@thoughtworks.com>
2018-03-22 09:49:37 +0800

    finish script

--- practice3.sql
+++ practice3.sql
@@ -3,4 +3,6 @@
 
 #先算出公司的平均薪资水平选出最高的薪资
 SELECT companyId, avg(salary) AS avgsal FROM Employee GROUP BY companyId ORDER BY avgsal DESC LIMIT 1;
+
+#结果：
 SELECT B.companyName, A.avgsal FROM (SELECT companyId, avg(salary) AS avgsal FROM Employee GROUP BY companyId) A INNER JOIN Company B ON A.companyId=B.id
---
+++ script.sh
@@ -0,0 +1,4 @@
+if [ ! -f query.sql ]; then
+    echo "query.sql不存在!";
+    exit 1;
+fi
